16e9e5418945885608131996f378cec4e492e633
Clement127001 <babiyonclement18@gmail.com>
2024-06-03 16:04:55 +0530

    add edit blog controller

--- Blog_App/server/controllers/blogsControllers.js
+++ Blog_App/server/controllers/blogsControllers.js
@@ -8,7 +8,6 @@ const User = require("../models/userModel");
 const getAllBlogs = async (req, res, next) => {
   try {
     const blogs = await Blog.find().sort({ updatedAt: -1 });
-
     res.status(200).json({ blogs });
   } catch (error) {
     return next(new HttpError("Could find blogs", 204));
@@ -20,17 +19,23 @@ const getSingleBlogs = async (req, res, next) => {
     const { id } = req.params;
     const blog = await Blog.findById(id);
 
-    if (!blog) return next(new HttpError("Blog not found", 204));
+    if (!blog) return next(new HttpError("Blog not found", 400));
 
     res.status(200).json({ blog });
   } catch (error) {
-    return next(new HttpError("Could find blog", 204));
+    return next(new HttpError("Could find blog", 400));
   }
 };
 
 const getAllblogOfAuthor = async (req, res, next) => {
-  const { id } = req.params;
-  res.send(`Get all blogs of author with id : ${id}`);
+  try {
+    const { id } = req.params;
+    const blogs = await Blog.find({ createdBy: id }).sort({ updatedAt: -1 });
+
+    if (!blogs) return next(new HttpError("No blogs found", 400));
+
+    res.status(200).json({ blogs });
+  } catch (error) {}
 };
 
 const getblogsWithCategory = async (req, res, next) => {
@@ -115,8 +120,111 @@ const createBlog = async (req, res, next) => {
 };
 
 const editBlog = async (req, res, next) => {
-  const { id } = req.params;
-  res.send(`Edited blog with given id : ${id}`);
+  try {
+    const { id } = req.params;
+    const { title, description, category } = req.body;
+    const { userId } = req.user;
+
+    if (!title || !description || !category) {
+      return next(new HttpError("Please fill out all values"));
+    }
+
+    if (description.trim().length <= 50)
+      return next(
+        new HttpError(
+          "Description is too short, description atleast have 50 characters"
+        )
+      );
+
+    const blogDetail = await Blog.findById(id);
+
+    //check whether the current login user is owner of that blog
+
+    if (blogDetail.createdBy == userId) {
+      console.log("Checking for the files");
+      if (!req.files) {
+        const updatedBlog = await Blog.findByIdAndUpdate(
+          id,
+          {
+            title,
+            description,
+            category,
+          },
+          { new: true }
+        );
+
+        console.log("updated blog without thumbnail");
+
+        return res.status(200).json({
+          blog: updatedBlog,
+          message: "Blog updated successfully",
+        });
+      } else {
+        console.log("New thumbnail found");
+        const oldPost = await Blog.findById(id);
+
+        try {
+          const filePath = path.join(
+            __dirname,
+            "..",
+            "uploads",
+            oldPost.thumbanil
+          );
+          await fs.promises.unlink(filePath);
+
+          console.log("unlinked the old thumbnail");
+
+          const thumbnail = req.files.thumbnail;
+
+          if (thumbnail.size > 2000000)
+            return next(
+              new HttpError("Thumbnail size should be less than 2mb", 400)
+            );
+
+          console.log("thumbnail passed");
+
+          const filename = thumbnail.name;
+          const splittedFilename = filename.split(".");
+          const newFileName =
+            splittedFilename[0] +
+            uuid() +
+            "." +
+            splittedFilename[splittedFilename.length - 1];
+
+          console.log("New file name : " + newFileName);
+
+          thumbnail.mv(
+            path.join(__dirname, "..", "uploads", newFileName),
+            async (err) => {
+              if (err) return next(new HttpError(err));
+
+              console.log("Moved the new thumbnail");
+
+              const updatedBlog = await Blog.findByIdAndUpdate(
+                id,
+                { title, description, category, thumbanil: newFileName },
+                { new: true }
+              );
+
+              if (!updatedBlog)
+                return next(
+                  new HttpError("Thumbnail couldn't be changed", 422)
+                );
+
+              return res.status(200).json({
+                blog: updatedBlog,
+                message: "Blog updated successfully",
+              });
+            }
+          );
+        } catch (err) {
+          return next(new HttpError("Error moving the thumbnail file", 500));
+        }
+      }
+    }
+  } catch (err) {
+    return next(new HttpError("Couldn't update the blog"));
+  }
 };
 
 const deleteBlog = async (req, res, next) => {
